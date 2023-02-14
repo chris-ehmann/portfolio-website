55a1d44c460d049fe4cd3a0b0785d954b2d5b1b1
Chris <chrisehmann@uvic.ca>
2023-02-14 12:28:36 -0800

    Minor changes

--- public/js/wave.js
+++ public/js/wave.js
@@ -1,20 +1,28 @@
 let waveEmoji = document.getElementById("wave-emoji");
 let rotation = 15;
-let loc = 0;
+let count = 0;
 
 function waveLeft() {
-    rotation += -1;
+    count++;
+    rotation += -1.15;
     waveEmoji.style.transform = "rotate(" + rotation + "deg)";
     if(rotation >= 0) {
         requestAnimationFrame(waveLeft);
     }
     else {
-        requestAnimationFrame(waveRight);
+        if(count >= 400) {
+            count = 0;
+            setTimeout(waveRight, 5000);
+        }
+        else {
+            requestAnimationFrame(waveRight);
+        }
     }
 }
 
 function waveRight() {
-    rotation += 1;
+    rotation += 1.15;
+    count++;
     waveEmoji.style.transform = "rotate(" + rotation + "deg)";
     if(rotation <= 30) {
         requestAnimationFrame(waveRight);
